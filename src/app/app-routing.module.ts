6a1a5ecf40edb1e21d25e28996d6132618710f57
mahaabid2001 <maha.abid@insat.ucar.tn>
2025-02-04 17:52:07 +0100

    implement-auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 import { LandingComponent } from './pages/landing/landing.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { StoresComponent } from './pages/stores/stores.component';
 import { SurpriseBagsComponent } from './pages/surprise-bags/surprise-bags.component';
 import { RoleGuard } from './shared/guards/auth.guard';
-import { StoreDashboardComponent } from './store-admin/store-dashboard/store-dashboard.component';
 import { BagsComponent } from './store-admin/bags/bags.component';
+import { StoreDashboardComponent } from './store-admin/store-dashboard/store-dashboard.component';
 import { StoreRegistrationComponent } from './store-registration/store-registration.component';
 
 const routes: Routes = [
